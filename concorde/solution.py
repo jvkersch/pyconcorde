6748412cec3d42ecc7bbc3fe3a7b624ccda32b0a
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2023-06-23 22:25:05 +0900

    MAINT: Add optimal_value property

--- concorde/solution.py
+++ concorde/solution.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import re
 
 
 @dataclass
@@ -7,23 +8,33 @@ class Solution:
 
     tour: list[int]
 
+    output: str
+
     @classmethod
-    def from_file(cls, fname):
+    def from_file(cls, fname, output=""):
         """Create a solution from a Concorde ``.sol`` file.
 
         Parameters
         ----------
         fname : str
             Path of the ``.sol`` file.
+        output : str
+            Output from the concorde run.
 
         """
         with open(fname) as fp:
             _, tour = _read_sol_file(fp)
-        return cls(tour=tour)
+        return cls(tour=tour, output=output)
 
     def __str__(self):
         """Create string representation of the tour."""
         return f"Tour on {len(self.tour)} nodes"
+
+    @property
+    def optimal_value(self):
+        """Return optimal tour value."""
+        value = _extract_value(self.output, "Optimal Solution: ")
+        return float(value)
 
 
 def _read_sol_file(fp):
@@ -32,3 +43,10 @@ def _read_sol_file(fp):
     for line in fp:
         nodes.extend(int(n) for n in line.split())
     return n_nodes, nodes
+
+
+def _extract_value(s, key):
+    m = re.search(f"{key}(.+)", s)
+    if not m:
+        raise RuntimeError(f"Key {key!r} not found in string {s!r}")
+    return m.groups()[0]
