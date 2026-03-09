a5f52b981926fe2d2d0a07af0f54a389c35caf76
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2026-03-09 09:06:28 +0900

    build: switch to uv for dependency management
    Move project metadata from setup.py to pyproject.toml, keep setup.py
    for custom Concorde/QSOpt build logic only. Add uv.lock and
    .python-version.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-""" setup command to build Concorde Cython wrappers.
+"""Custom build command to download and compile Concorde/QSOpt.
 
 By default, the setup script will download the QSOpt linear solver, and
 download and compile Concorde. If you have either one of these packages
@@ -13,22 +13,15 @@ not set these variables (and rely on the downloaded Concorde) or set
 both of them. Setting only one will not work as intended.
 
 """
-from __future__ import print_function
 
 import os
 from os.path import exists, join as pjoin
 import platform
 import shutil
 import subprocess
+import urllib.request
 
-try:
-    import urllib.request
-
-    urlretrieve = urllib.request.urlretrieve
-except ImportError:  # python 2
-    from urllib import urlretrieve
-
-from setuptools import find_packages, setup, Extension
+from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext as _build_ext
 
 from Cython.Build import cythonize
@@ -59,10 +52,7 @@ CONCORDE_SRC = "https://www.math.uwaterloo.ca/tsp/concorde/downloads/codes/src/c
 
 def _safe_makedirs(*paths):
     for path in paths:
-        try:
-            os.makedirs(path)
-        except os.error:
-            pass
+        os.makedirs(path, exist_ok=True)
 
 
 def download_concorde_qsopt():
@@ -74,12 +64,12 @@ def download_concorde_qsopt():
         print("qsopt is missing, downloading")
         machine = platform.machine()
         qsopt_a_url, qsopt_h_url = QSOPT_LOCATION[platform.system()][machine]
-        urlretrieve(qsopt_a_url, qsopt_a_path)
-        urlretrieve(qsopt_h_url, qsopt_h_path)
+        urllib.request.urlretrieve(qsopt_a_url, qsopt_a_path)
+        urllib.request.urlretrieve(qsopt_h_url, qsopt_h_path)
     concorde_src_path = pjoin("build", "concorde.tgz")
     if not exists(concorde_src_path):
         print("concorde is missing, downloading")
-        urlretrieve(CONCORDE_SRC, concorde_src_path)
+        urllib.request.urlretrieve(CONCORDE_SRC, concorde_src_path)
 
 
 def _run(cmd, cwd):
@@ -111,7 +101,7 @@ def build_concorde():
         shutil.copyfile("build/concorde/concorde.h", "data/concorde.h")
 
 
-class build_ext(_build_ext, object):
+class build_ext(_build_ext):
     """Build command that downloads and installs Concorde, if not found."""
 
     def run(self):
@@ -121,7 +111,7 @@ class build_ext(_build_ext, object):
         else:
             print("Using external Concorde/QSOpt")
 
-        super(build_ext, self).run()
+        super().run()
 
     @property
     def has_external_concorde(self):
@@ -130,11 +120,11 @@ class build_ext(_build_ext, object):
         return bool(qsopt_dir) and bool(concorde_dir)
 
 
-class ConcordeExtension(Extension, object):
+class ConcordeExtension(Extension):
     """Extension that sets Concorde/QSOpt lib/include args."""
 
     def __init__(self, *args, **kwargs):
-        super(ConcordeExtension, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         qsopt_dir = os.environ.get("QSOPT_DIR", "data")
         concorde_dir = os.environ.get("CONCORDE_DIR", "data")
         self.include_dirs.append(concorde_dir)
@@ -144,7 +134,6 @@ class ConcordeExtension(Extension, object):
 
 
 setup(
-    name="pyconcorde",
     ext_modules=cythonize(
         [
             ConcordeExtension(
@@ -154,28 +143,6 @@ setup(
             )
         ]
     ),
-    version="0.1.0",
-    install_requires=[
-        "cython>=0.22.0",
-        "numpy>=1.21.0",
-        "tsplib95",
-    ],
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    license="BSD",
-    author="Joris Vankerschaver",
-    author_email="joris.vankerschaver@gmail.com",
-    url="https://github.com/jvkersch/pyconcorde",
-    description="Cython wrappers for the Concorde TSP library",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Science/Research",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "License :: OSI Approved :: BSD License",
-    ],
     cmdclass={
         "build_ext": build_ext,
     },
